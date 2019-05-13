6b85678dfcbb44335a8d1d3518a9b0d864e9bf90
Lucas Possatti <lucas_possatti@hotmail.com>
2019-05-13 00:28:11 -0300

    Fix carbon not restarting because of pid file

--- docker/graphite/run_carbon_cache.sh
+++ docker/graphite/run_carbon_cache.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-# Just an idea. Maybe I should copy conf files to the right folder, and then
-# overwrite values with the values from environment variables...
+
+## I was having issues with carbon cache. At times it would die, but left the pid file.
+## When docker tries to restart it, carbon says it's already running because of the pid
+## file, even though it's not.
+rm -f ${GRAPHITE_ROOT}/storage/*.pid
+
+/opt/graphite/bin/carbon-cache.py --nodaemon start
+
